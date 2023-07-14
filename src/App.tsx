f2173ee039c1fb86ddf37dba148240df91f5a477
Nike073i <nike073i@mail.ru>
2023-07-14 18:42:27 +0400

    Организация стилей, создание темы и ее контекста

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 import AboutPageAsync from "./pages/AboutPage/AboutPage.async";
 import MainPageAsync from "./pages/MainPage/MainPage.async";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
 
 function App() {
+    const { theme, toggleTheme } = useTheme()
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to='/'>Главная</Link>
             <Link to='/about'>О сайте</Link>
             <Suspense fallback={<div>Loading</div>}>
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import React, { useMemo, useState } from 'react';
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
+
+function ThemeProvider({ children }: React.PropsWithChildren<{}>) {
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+    };
+    const defaultProps = useMemo(() => ({
+        theme: theme,
+        setTheme: toggleTheme
+    }), [theme]);
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}
+
+export default ThemeProvider;

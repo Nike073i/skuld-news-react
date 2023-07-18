77830a9931a341d179f813974b653f3cebe78cc2
Nike073i <nike073i@mail.ru>
2023-07-18 15:16:11 +0400

    Подключил плагин отслеживания размера бандла

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -15,6 +16,9 @@ function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInst
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
         }),
     ];
 }

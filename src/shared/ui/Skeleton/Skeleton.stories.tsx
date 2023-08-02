77933b02e8adfe244652938976e7dc793a47b824
Nike073i <nike073i@mail.ru>
2023-08-02 20:10:01 +0400

    Создаем UI-кит скелетона

---
+++ src/shared/ui/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Skeleton } from './Skeleton';
+
+const meta = {
+    title: 'shared/Skeleton',
+    component: Skeleton,
+} satisfies Meta<typeof Skeleton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const FullLine: Story = {
+    args: {
+        width: '100%',
+        height: 200,
+    },
+};
+
+export const FullLineDark: Story = {
+    args: {
+        width: '100%',
+        height: 200,
+    },
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
+
+export const Circle: Story = {
+    args: {
+        border: '50%',
+        width: 100,
+        height: 100,
+    },
+};
+export const CircleDark: Story = {
+    args: {
+        border: '50%',
+        width: 100,
+        height: 100,
+    },
+    decorators: [ThemeDecorator(Theme.DARK)],
+};

024ed1ec0664a3b9d5ded05460593ad8c0e1a564
Nike073i <nike073i@mail.ru>
2023-08-08 17:00:01 +0400

    Хук бесконечной ленты (бесконечный скролл)

---
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -0,0 +1,41 @@
+import { MutableRefObject, useEffect } from 'react';
+
+export interface UseInfiniteScrollOptions {
+    callback?: () => void;
+    triggerRef: MutableRefObject<HTMLElement>;
+    containerRef: MutableRefObject<HTMLElement>;
+}
+
+export function useInfiniteScroll(props: UseInfiniteScrollOptions) {
+    const {
+        callback,
+        triggerRef,
+        containerRef,
+    } = props;
+
+    useEffect(() => {
+        const triggerElement = triggerRef.current;
+        let observer: IntersectionObserver | null;
+
+        if (callback) {
+            const options = {
+                root: containerRef.current,
+                rootMargin: '0px',
+                threshold: 1.0,
+            };
+            observer = new IntersectionObserver(([entry]) => {
+                if (entry.isIntersecting) {
+                    callback();
+                }
+            }, options);
+            observer.observe(triggerElement);
+        }
+
+        return () => {
+            if (observer && triggerElement) {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                observer.unobserve(triggerElement);
+            }
+        };
+    }, [callback, containerRef, triggerRef]);
+}

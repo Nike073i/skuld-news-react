505fb5a24e1ffdb3be8c0dca4ee004c373b4c520
Nike073i <nike073i@mail.ru>
2023-08-14 19:18:48 +0400

    Подключаем библиотеку для unit html репортов

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -36,6 +36,14 @@ const config: Config = {
         __API__: '',
         __PROJECT__: 'jest',
     },
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+        }],
+    ],
 };
 
 export default config;

2dcbffa673089c3041c9ea1b08f2e154574178b3
Nike073i <nike073i@mail.ru>
2023-08-20 21:33:18 +0400

    Плагин на проверку неиспользуемых импортов

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
         'i18next',
         'react-hooks',
         'ulbi-tv-plugin',
+        'unused-imports',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -78,6 +79,7 @@ module.exports = {
                 alias: '@',
                 ignoreImportPatterns: ['**/StoreProvider', '**/testing'],
             }],
+        'unused-imports/no-unused-imports': 'error',
     },
     globals: {
         __IS_DEV__: true,

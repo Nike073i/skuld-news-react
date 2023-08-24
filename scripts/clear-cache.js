1c291fdde988bc5b0750a4dab4bc7e1f60472c0c
Nike073i <nike073i@mail.ru>
2023-08-25 02:03:56 +0400

    Проверка существования папки .cache

--- scripts/clear-cache.js
+++ scripts/clear-cache.js
@@ -2,10 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 const cacheDir = path.join(__dirname, '..', 'node_modules', '.cache');
-fs.rmdir(cacheDir, { recursive: true }, (err) => {
+fs.stat(cacheDir, (err) => {
     if (err) {
-        console.error('An error occurred while clearing the cache');
-        return;
+        if (err.code === 'ENOENT') {
+            console.log('Cache folder missing');
+        } else {
+            console.error('An error occurred while clearing the cache');
+        }
+    } else {
+        fs.rmdir(cacheDir, { recursive: true }, (err) => {
+            if (err) {
+                console.error('An error occurred while clearing the cache');
+                return;
+            }
+            console.log('Cache has been cleared');
+        });
     }
-    console.log('Cache has been cleared');
 });

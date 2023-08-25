a8c5b5f86c95b6d33e173694e95c725a244167b4
Nike073i <nike073i@mail.ru>
2023-08-25 17:24:43 +0400

    добавление аргументов в buildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,10 +1,18 @@
 import { useSelector } from 'react-redux';
+// eslint-disable-next-line ulbi-tv-plugin/layer-imports
 import { StateSchema } from '@/app/providers/StoreProvider';
 
-type Selector<T> = (state: StateSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+type Selector<T, TArgs extends any[]> = (
+    state: StateSchema,
+    ...args: TArgs
+) => T;
+type Hook<T, TArgs extends any[]> = (...args: TArgs) => T;
+type Result<T, TArgs extends any[]> = [Hook<T, TArgs>, Selector<T, TArgs>];
 
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
-    const useSelectorHook = () => useSelector(selector);
+export function buildSelector<T, TArgs extends any[]>(
+    selector: Selector<T, TArgs>,
+): Result<T, TArgs> {
+    const useSelectorHook: Hook<T, TArgs> = (...args: TArgs) =>
+        useSelector((state: StateSchema) => selector(state, ...args));
     return [useSelectorHook, selector];
 }

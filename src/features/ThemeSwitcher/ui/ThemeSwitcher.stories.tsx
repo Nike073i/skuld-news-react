a821cd90b41a9a6d2ffa903c386708252009ec61
Nike073i <nike073i@mail.ru>
2023-08-31 18:20:55 +0400

    Фикс сторибука

--- src/features/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -2,20 +2,23 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator';
 import { ThemeSwitcher } from './ThemeSwitcher';
 import { Theme } from '@/shared/consts/theme';
+import { FeaturesFlagsDecorator } from '@/shared/config/storybook/FeaturesFlagsDecorator';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator';
 
 const meta = {
     title: 'shared/ThemeSwitcher',
     component: ThemeSwitcher,
+    decorators: [StoreDecorator({})],
 } satisfies Meta<typeof ThemeSwitcher>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Light: Story = {
-    args: {},
-};
+export const Light: Story = {};
 
 export const Dark: Story = {
-    args: {},
-    decorators: [ThemeDecorator(Theme.DARK)],
+    decorators: [
+        ThemeDecorator(Theme.DARK),
+        FeaturesFlagsDecorator({ isAppRedesigned: false }),
+    ],
 };

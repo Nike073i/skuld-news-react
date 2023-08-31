7bf29bbccf5d1f37682e91753b23e805335d9733
Nike073i <nike073i@mail.ru>
2023-08-31 13:58:16 +0400

    Редизайн скелетона плиточных статей и скролбара

--- src/entities/Article/ui/ArticleListItem/ArticleListItemSkeleton.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItemSkeleton.tsx
@@ -5,7 +5,7 @@ import { Card as CardRedesigned } from '@/shared/ui/redesigned/Card';
 import { Skeleton as SkeletonDeprecated } from '@/shared/ui/deprecated/Skeleton';
 import { ArticleView } from '../../model/consts/consts';
 import cls from './ArticleListItem.module.scss';
-import { toggleFeatures } from '@/shared/lib/features';
+import { ToggleFeatures, toggleFeatures } from '@/shared/lib/features';
 import { Skeleton as SkeletonRedesigned } from '@/shared/ui/redesigned/Skeleton';
 
 interface ArticleListItemSkeletonProps {
@@ -69,23 +69,51 @@ export const ArticleListItemSkeleton = memo(
                 </div>
             );
         }
-        return (
-            <div
-                className={classNames(mainClass, mods, [className, cls[view]])}
-            >
-                <Card className={cls.card}>
-                    <div className={cls.imageWrapper}>
+
+        const cardContent = (
+            <>
+                <ToggleFeatures
+                    feature="isAppRedesigned"
+                    on={
                         <Skeleton
-                            width={200}
-                            height={200}
+                            width="100%"
+                            height={150}
+                            border="32px"
                             className={cls.image}
                         />
-                    </div>
-                    <div className={cls.infoWrapper}>
-                        <Skeleton width={130} height={16} />
-                    </div>
-                    <Skeleton width={150} height={16} className={cls.title} />
-                </Card>
+                    }
+                    off={
+                        <div className={cls.imageWrapper}>
+                            <Skeleton
+                                width={200}
+                                height={200}
+                                className={cls.image}
+                            />
+                        </div>
+                    }
+                />
+                <div className={cls.infoWrapper}>
+                    <Skeleton width={130} height={16} />
+                </div>
+                <Skeleton width={150} height={16} className={cls.title} />
+            </>
+        );
+
+        return (
+            <div className={classNames(mainClass, {}, [className, cls[view]])}>
+                <ToggleFeatures
+                    feature="isAppRedesigned"
+                    on={
+                        <CardRedesigned border="round" className={cls.card}>
+                            {cardContent}
+                        </CardRedesigned>
+                    }
+                    off={
+                        <CardDeprecated className={cls.card}>
+                            {cardContent}
+                        </CardDeprecated>
+                    }
+                />
             </div>
         );
     },

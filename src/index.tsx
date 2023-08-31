5c3a88f508cab660aa11a31276a6cf31f846512a
Nike073i <nike073i@mail.ru>
2023-08-31 13:38:37 +0400

    Фикс смены дизайна при логауте (Костыль)

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { StoreProvider } from '@/app/providers/StoreProvider';
 import '@/app/styles/index.scss';
 import '@/shared/config/i18n/i18n';
 import App from '@/app/App';
+import { ForceUpdateProvider } from './shared/lib/render/forceUpdate';
 
 const container = document.getElementById('root');
 if (!container) {
@@ -19,9 +20,11 @@ root.render(
     <BrowserRouter>
         <StoreProvider>
             <ErrorBoundary>
-                <ThemeProvider>
-                    <App />
-                </ThemeProvider>
+                <ForceUpdateProvider>
+                    <ThemeProvider>
+                        <App />
+                    </ThemeProvider>
+                </ForceUpdateProvider>
             </ErrorBoundary>
         </StoreProvider>
     </BrowserRouter>,
